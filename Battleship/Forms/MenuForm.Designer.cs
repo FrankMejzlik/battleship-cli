f246630e09608d3ba013948b6cd273cea06cf3ac
Frank Mejzlik <frankmejzlik@gmail.com>
2020-05-01 21:16:26 +0200

    Add project files.

---
+++ Battleship/Forms/MenuForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Battleship.Forms
+{
+    partial class MenuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startGameButton = new System.Windows.Forms.Button();
+            this.joinGameButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startGameButton
+            // 
+            this.startGameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.startGameButton.Location = new System.Drawing.Point(10, 10);
+            this.startGameButton.Name = "startGameButton";
+            this.startGameButton.Size = new System.Drawing.Size(460, 130);
+            this.startGameButton.TabIndex = 0;
+            this.startGameButton.Text = "Start Game";
+            this.startGameButton.UseVisualStyleBackColor = true;
+            // 
+            // joinGameButton
+            // 
+            this.joinGameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.joinGameButton.Location = new System.Drawing.Point(10, 150);
+            this.joinGameButton.Name = "joinGameButton";
+            this.joinGameButton.Size = new System.Drawing.Size(460, 130);
+            this.joinGameButton.TabIndex = 1;
+            this.joinGameButton.Text = "Join Game";
+            this.joinGameButton.UseVisualStyleBackColor = true;
+            // 
+            // MenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 283);
+            this.Controls.Add(this.joinGameButton);
+            this.Controls.Add(this.startGameButton);
+            this.Name = "MenuForm";
+            this.Text = "Battleship";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startGameButton;
+        private System.Windows.Forms.Button joinGameButton;
+    }
+}
